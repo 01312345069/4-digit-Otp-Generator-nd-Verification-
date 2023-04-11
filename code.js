e8623ca84e7a48c11eba61cee3a131e08316f59e
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2023-04-11 15:38:05 +0600

    create clear and backspace function

--- code.js
+++ code.js
@@ -21,13 +21,22 @@ document.getElementById('generate-Otp').addEventListener('click', function () {
 
 })
 
-document.getElementById('calculator').addEventListener('click', function () {
+document.getElementById('calculator').addEventListener('click', function (event) {
     const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
     if (isNaN(number)) {
-        console.log(number);
-    } else {
-        const typedNumberField = document.getElementById('typed-numbers');
-        const previousTypedNumber = typedNumberField.value;
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else {
         const newTypedNumber = previousTypedNumber + number;
         typedNumberField.value = newTypedNumber;
     }
